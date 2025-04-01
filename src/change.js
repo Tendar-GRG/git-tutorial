8a3579183c0fb3aee94a4732d2fb4bb713d2f37a
tendar <gtgtendar@gmail.com>
2025-04-01 22:09:11 +0530

    version 3

--- src/change.js
+++ src/change.js
@@ -1,3 +1,3 @@
 console.log('change');
 console.log("i am tandar");
-console.log('version 2');
+console.log('version 3');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log('hi tendar');
-console.log("version 2");
+console.log("version 3");
